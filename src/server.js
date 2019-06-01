afa0bdb418ec369f58cbf2b03af60ee82cff671f
Robert Luiz <robert.tiweb@gmail.com>
2019-05-31 23:24:10 -0300

    Criar estrutura inicial closes #1

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+class App {
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV != 'production'
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares () {
+    this.express.use(express.json())
+  }
+
+  routes () {
+    this.express.use(require('./routes'))
+  }
+}
+
+module.exports = new App().express
