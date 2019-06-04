f0a60939d59bd6eb122805a99f2d11ec47ef088e
Robert Luiz <robert.tiweb@gmail.com>
2019-06-04 20:40:31 -0300

    Schema de usuário closes #3

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('User', UserSchema)
