82f3e27e3f73ce1bac0f9a4f6c4a0fb68620e395
Robert Luiz <robert.tiweb@gmail.com>
2019-06-04 20:08:00 -0300

    mongoose closes #2

---
+++ src/config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  uri: 'mongodb://localhost:28018/gonode03'
+}
--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 const express = require('express')
-const path = require('path')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
-    this.isDev = process.env.NODE_ENV != 'production'
+    this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.middlewares()
     this.routes()
+  }
+
+  database () {
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 
   middlewares () {
